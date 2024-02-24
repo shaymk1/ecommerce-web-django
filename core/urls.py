ddd5afe187ed41e5472a92f4d31d9b58b95d99fb
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-24 11:27:09 +0200

    created category app models and registred it in the admin panel

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    # path('', include('category.urls')),
     path((''), views.home, name='home'),
     path(('store/'), views.store, name='store'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
