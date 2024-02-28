3b05c6f68dbf174f76123e56e0734fb6e0d6f575
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-28 13:35:24 +0200

    modified the images to image in the store models and migrated, queried the models via the core views.py home function, and in the home template, dynamically coded the fields with a for loop in

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
-    return render(request, 'store/index.html')
+    products = Product.objects.all().filter(is_available=True)
+    context = {"products": products}
+    return render(request, "store/home.html", context)
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    return render(request, "store/store.html")
