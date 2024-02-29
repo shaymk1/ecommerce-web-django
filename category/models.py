38bc13fcdf0241bf073d071a282f905c6be00c9a
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-29 10:31:35 +0200

    created context_processors.py in the category app to help me list products under categories.I created a get_url a function in the store model file to get the url of the products when clicked on. i modified the navbar and store html files to reflect the categories and urls dynamically

--- category/models.py
+++ category/models.py
@@ -8,15 +8,18 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
-    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+    cat_image = models.ImageField(upload_to="photos/categories", blank=True)
 
     class Meta:
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
+    # for the category to show products when clicked on:
     def get_url(self):
-        return reverse('products_by_category', args=[self.slug])
+        return reverse("products_by_category", args=[self.slug])
 
     def __str__(self):
         return self.category_name
+
+
 # Create your models here.
