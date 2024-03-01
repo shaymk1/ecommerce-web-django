b6b5f590a25465f5247a45ecd0377bc4d789bbdf
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-03-01 12:08:36 +0200

    modified home, navbar, store html template to include the redirect to product details or store or home

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
- 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+    path("admin/", admin.site.urls),
     path('store/', include('store.urls')),
-    path((''), views.home, name='home'),
-    # path(('store/'), views.store, name='store'),
+    path((""), views.home, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
